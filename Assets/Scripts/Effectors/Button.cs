f931be015a9ef83d4a2059c3164efa6d16757c3f
unknown <JIHlucky7@gmail.com>
2020-10-03 16:30:19 -0500

    Added Doors

---
+++ Assets/Scripts/Effectors/Button.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : Effector
+{
+    [SerializeField] LayerMask layerMask;
+
+    bool pressed;
+
+    public bool Pressed()
+    {
+        return pressed;
+    }
+
+    private void Start()
+    {
+        pressed = false;
+    }
+
+    protected override void EnterAction(GameObject other)
+    {
+        if (layerMask == (layerMask | (1 << other.layer)))
+        {
+            pressed = true;
+        }
+    }
+
+    protected override void ExitAction(GameObject other)
+    {
+        if (layerMask == (layerMask | (1 << other.layer)))
+        {
+            pressed = false;
+        }
+    }
+}
