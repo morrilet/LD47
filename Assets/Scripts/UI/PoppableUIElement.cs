46ee7a90a88366f91989afff2db0b914898192fd
Ethan <findwaldoFTLOG@gmail.com>
2020-10-04 14:03:11 -0400

    Poppable UI elements, world UI

---
+++ Assets/Scripts/UI/PoppableUIElement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PoppableUIElement : MonoBehaviour
+{
+    [SerializeField] private RectTransform localTransform;
+    [SerializeField] private Image localImage;
+
+    [Space, Header("Effect")]
+    [SerializeField] private float effectDuration;
+    [SerializeField] private AnimationCurve heightCurve;
+    [SerializeField] private AnimationCurve transparencyCurve;
+
+    private float timer = 0.0f;
+    private Vector3 tempVector;
+    private Color tempColor;
+
+    private void Update() {
+        UpdateHeight();
+        UpdateTransparency();
+
+        if (timer > effectDuration) {
+            GameObject.Destroy(this.gameObject);
+        }
+        timer += Time.deltaTime;
+    }
+
+    private void UpdateHeight() {
+        tempVector = localTransform.anchoredPosition;
+        tempVector.y = heightCurve.Evaluate(timer / effectDuration);
+        localTransform.anchoredPosition = tempVector;
+    }
+
+    private void UpdateTransparency() {
+        tempColor = localImage.color;
+        tempColor.a = transparencyCurve.Evaluate(timer / effectDuration);
+        localImage.color = tempColor;
+    }
+}
---
+++ Assets/Scripts/UI/WorldUIController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public struct PoppableUIElementPrefab
+{
+    public string name;
+    public GameObject prefab;
+}
+
+public class WorldUIController : MonoBehaviour
+{
+    [SerializeField] private PoppableUIElementPrefab[] poppableUIElements;
+
+    public void SpawnPoppableUIElement(string name) {
+        PoppableUIElementPrefab element = GetPoppableUIElement(name);
+        GameObject.Instantiate(element.prefab, transform.position, Quaternion.identity, transform);
+    }
+
+    private PoppableUIElementPrefab GetPoppableUIElement(string name) {
+        for (int i = 0; i < poppableUIElements.Length; i++) {
+            if (poppableUIElements[i].name == name)
+                return poppableUIElements[i];
+        }
+
+        Debug.LogWarning($"PoppableUIElement with name '{name}' not found on GameObject called '{gameObject.name}'");
+        return new PoppableUIElementPrefab();
+    }
+
+    // For debugging...
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.P)) {
+            SpawnPoppableUIElement("Test");
+        }
+    }
+}
