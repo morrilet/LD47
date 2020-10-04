2a52408bfb49331df2676b239871d6c098a989ff
Ethan <findwaldoFTLOG@gmail.com>
2020-10-04 17:14:08 -0400

    Add UMotion, WIP: Sammy updates

---
+++ Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class CharacterAnimator : MonoBehaviour {
+    
+    [SerializeField] private SammyController controller;
+    [SerializeField] private Animator animator;
+
+    
+}
