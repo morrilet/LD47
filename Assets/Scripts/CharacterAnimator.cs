ac6e54e9d06285ae95bc8df2da61dcb59f5a8f4c
Ethan <findwaldoFTLOG@gmail.com>
2020-10-05 01:51:49 -0400

    WIP: Run animation

--- Assets/Scripts/CharacterAnimator.cs
+++ Assets/Scripts/CharacterAnimator.cs
@@ -7,4 +7,8 @@ public class CharacterAnimator : MonoBehaviour {
 
     // TODO: Translate character movements into animation triggers. Alternatively, feed character controller data to
     //       the animator and it'll handle it's own transitions. That's probably best.
+
+    void Update() {
+        animator.SetFloat("VelocityX", controller.GetVelocity().x);
+    }
 }
