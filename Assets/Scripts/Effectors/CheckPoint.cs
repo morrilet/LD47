18d0049d1d784ed77c538b8d6a0a59692bc1c130
unknown <JIHlucky7@gmail.com>
2020-10-04 23:29:28 -0500

    Adjusted platform placement restrictions, added player reset button and checkpoints.

---
+++ Assets/Scripts/Effectors/CheckPoint.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : Effector
+{
+    protected override void EnterAction(GameObject other)
+    {
+        GameManager.instance.NewCheckpoint();
+    }
+}
